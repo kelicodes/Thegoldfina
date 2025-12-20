6f02d4f17e18e344d76b6f5a54c8211cb4131abd
kelicodes <kelidevske@gmail.com>
2025-12-20 19:18:22 +0300

    o:

--- Controllers/Avatarcontroller.js
+++ Controllers/Avatarcontroller.js
@@ -1,9 +1,11 @@
 import { generateAnimeAvatar } from "../Middleware/Anime.js";
 import Avatar from "../Models/Avatar.js";
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+
+
 import fs from "fs";
 
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
