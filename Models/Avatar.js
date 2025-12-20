597592221aa5449d099907d898e8227def5992f5
kelicodes <kelidevske@gmail.com>
2025-12-20 17:09:46 +0300

    o

--- Models/Avatar.js
+++ Models/Avatar.js
@@ -2,15 +2,21 @@ import mongoose from "mongoose";
 
 const AvatarSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  imageUrl: { type: String,  },        // Anime/Ghibli avatar URL
-  originalImage: { type: String,required: true },                   // Original uploaded image URL
-  height: { type: Number },                          // optional
-  weight: { type: Number },                          // optional
-  bodyShape: { type: String },                       // optional, e.g., 'apple', 'pear'
-  shoeSize: { type: Number },                        // optional
-  favColor: { type: String },                        // optional
-  otherQuestions: { type: Map, of: String },        // optional, e.g., { "favoriteFood": "Pizza", "hobby": "Reading" }
+
+  imageUrl: { type: String },        // generated avatar later
+  originalImage: { type: String, required: true },
+
+  height: Number,
+  weight: Number,
+  clothingSize: String,
+  shoeSize: Number,
+  favoriteColor: String,
+  stylePreference: String,
+  budgetRange: String,
+  bodyType: String,
+
   createdAt: { type: Date, default: Date.now },
 });
+
 
 export default mongoose.model("Avatar", AvatarSchema);
