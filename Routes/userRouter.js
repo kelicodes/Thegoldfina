63d7aec7a517aa51f3b98197cc9a6600e2a83d1c
kelicodes <kelidevske@gmail.com>
2025-12-21 10:47:17 +0300

    UserAuth

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,4 +1,5 @@
 import {userRegistration,userSignin,logout,fecthme} from "../Controllers/userController.js"
+import UserAuth from "../Middleware/userAuth.js"
 import express from "express"
 
 const userRouter=express.Router()
@@ -6,7 +7,7 @@ const userRouter=express.Router()
 userRouter.post('/login',userSignin)
 userRouter.post('/reg',userRegistration)
 userRouter.post("/logout",logout)
-userRouter.get("/me",fecthme)
+userRouter.get("/me",UserAuth,fecthme)
 
 
 export default userRouter
